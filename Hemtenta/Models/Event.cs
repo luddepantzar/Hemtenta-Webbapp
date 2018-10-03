bfbc60f670f5e1d4c0f4dfa9d38d74cfed7ccc59
luddepantzar <33481596+luddepantzar@users.noreply.github.com>
2018-10-03 13:48:44 +0200

    Snart klart

--- Hemtenta/Models/Event.cs
+++ Hemtenta/Models/Event.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,11 +9,22 @@ namespace Hemtenta.Models
     public class Event
     {
         public int Id { get; set; }
+
+        [MaxLength(20), Required]
         public string Title { get; set; }
+
+        [DataType(DataType.DateTime), Required]
         public DateTime Date { get; set; }
+
+        [Required]
         public string Location { get; set; }
+
+        [MaxLength(250), Required]
         public string Description { get; set; }
+
+        [DataType(DataType.ImageUrl), Required]
         public string ImgUrl { get; set; }
+
         public int ReadCount { get; set; }
 
         public virtual List<UserIntrest> UserIntrests { get; set; }
--- Hemtenta/RequestObj/RegisterRequestObj.cs
+++ Hemtenta/RequestObj/RegisterRequestObj.cs
@@ -8,19 +8,19 @@ namespace Hemtenta.RequestObj
 {
     public class RegisterRequestObj
     {
-        [MinLength(3), MaxLength(15), Required]
+        [MinLength(3), MaxLength(15), Display(Name = "Användarnamn"), Required]
         public string UserName { get; set; }
 
         [EmailAddress(ErrorMessage = "Det måste vara en giltig email"), DataType(DataType.EmailAddress), Required]
         public string Email { get; set; }
 
-        [DataType(DataType.Password), Required]
+        [DataType(DataType.Password), Display(Name = "Lösenord"), Required]
         public string Password { get; set; }
 
-        [DataType(DataType.Password), Required, Display(Name = "Confirm Password"), Compare("Password")]
+        [DataType(DataType.Password), Required, Display(Name = "Bekräfta lösenord"), Compare("Password")]
         public string ConfirmPassword { get; set; }
 
-        [MinLength(10), MaxLength(200), Required]
+        [MinLength(10), MaxLength(200), Display(Name = "Deskription"), Required]
         public string Description { get; set; }
     }
 }
