7487131a885dac683a51123553c63a4ddec91fc3
luddepantzar <33481596+luddepantzar@users.noreply.github.com>
2018-10-03 10:02:23 +0200

    Added viewmodel

--- Hemtenta/ViewModel/CreateEventReviewViewModel.cs
+++ Hemtenta/ViewModel/CreateEventReviewViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Hemtenta.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,12 @@ namespace Hemtenta.ViewModel
 {
     public class CreateEventReviewViewModel
     {
+        public Event events { get; set; }
+        public Review reviews { get; set; }
+
+        public CreateEventReviewViewModel(Event Events)
+        {
+            events = Events;
+        }
     }
 }
